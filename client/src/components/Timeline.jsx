6719871d13396e97f006a06afb8e55b62c3afdb9
Pratyaksh Tyagi <55044774+pratyaksh123@users.noreply.github.com>
2021-01-05 20:45:19 +0530

    Added About Us Page

---
+++ client/src/components/Timeline.jsx
@@ -0,0 +1,18 @@
+import React, { PureComponent } from 'react';
+
+class TimeLine extends PureComponent {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	render() {
+		return (
+			<div>
+				<p>TimeLine</p>
+			</div>
+		);
+	}
+}
+
+export default TimeLine;
